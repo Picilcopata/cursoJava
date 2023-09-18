c1aacbc507cb14fc495fd3458d094c95a5c2de6e
João Paulo <144500373+Picilcopata@users.noreply.github.com>
2023-09-18 19:25:36 -0300

    Add files via upload

---
+++ exercicios/src/App.java
@@ -0,0 +1,10 @@
+import classe.*;
+public class App {
+    public static void main(String[] args) {
+        
+        //>>>TESTE DO PRODUTO<<<
+        Produto prod1 = new Produto("Leite", 1.50);
+        prod1.statusProduto();
+        
+    }
+}
---
+++ exercicios/src/classe/PrimeiroTrauma.java
@@ -0,0 +1,13 @@
+package classe;
+
+public class PrimeiroTrauma {
+    int a = 3; // não póde mexer aqui!
+    static int b = 4;
+    public static void main(String[] args) {
+        PrimeiroTrauma a1 = new PrimeiroTrauma();
+        System.out.println(a1.a);
+
+        System.out.println(b);
+    }
+    
+}
